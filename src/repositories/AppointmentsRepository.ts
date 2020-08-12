fe868e88324cc0076c7c68818106b87187f7c3f2
bivarz <paulo.bivar@gmail.com>
2020-08-11 22:30:55 -0300

    :tada: Editing

--- src/repositories/AppointmentsRepository.ts
+++ src/repositories/AppointmentsRepository.ts
@@ -7,12 +7,6 @@ interface CreateAppointmentDTO {
 }
 
 class AppointmentsRepository {
-  private appointments: Appointment[];
-
-  constructor() {
-    this.appointments = [];
-  }
-
   public all(): Appointment[] {
     return this.appointments;
   }
